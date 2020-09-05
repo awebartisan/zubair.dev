0c78823333706ff5d6ecc4b1f0be90b2bce812de
Zubair Mohsin <awebartisan@gmail.com>
2020-09-05 22:02:01 +0500

    Header and Nav

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,36 @@
-import Nav from '../components/nav'
+import Head from "next/head";
+import Link from "next/link";
 
-export default function IndexPage() {
+export default function Index() {
   return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-accent-1">
-          Next.js + Tailwind CSS
-        </h1>
+    <>
+      <Head>
+        <title>Zubair Mohsin</title>
+      </Head>
+      <div className='mx-auto mb-2'>
+        <div className='pt-8 lg:pt-12'>
+          <h1 className='text-3xl text-center text-accent-1'>Zubair Mohsin</h1>
+        </div>
       </div>
-    </div>
-  )
+      <div className='w-1/2 px-6 md:px-16 lg:px-32 mx-auto'>
+        <ul className='flex justify-between uppercase'>
+          <li>
+            <Link href='/blog'>
+              <a className='text-gray-600'>Blog</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/projects'>
+              <a className='text-gray-600'>Projects</a>
+            </Link>
+          </li>
+          <li>
+            <Link href='/contact'>
+              <a className='text-gray-600'>Contact</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 }
