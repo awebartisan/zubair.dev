a65c63233f77411774c4fc16e4e8446b914efb25
Zubair Mohsin <zubairmohsin33@gmail.com>
2021-01-20 11:45:03 +0500

    Package upgrades and layout adjustments

--- components/Layout.js
+++ components/Layout.js
@@ -1,59 +1,64 @@
-import Head from "next/head";
-import Link from "next/link";
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function Layout({ title, children }) {
-  return (
-    <>
-      <Head>
-        <title>Zubair Mohsin | {title}</title>
-      </Head>
-      <div className='container px-10'>
-        <div className='mb-2'>
-          <div className='pt-8 lg:pt-12'>
-            <h1 className='text-5xl'>
-              <Link href='/'>
-                <a>Zubair Mohsin</a>
-              </Link>
-            </h1>
-          </div>
-        </div>
-        <div className='w-1/2'>
-          <ul className='flex uppercase space-x-6'>
-            <li>
-              <Link href='https://dev.to/zubairmohsin33'>
-                <a
-                  target='_blank'
-                  className={
-                    title === "Blog" ? "text-gray-800" : "text-gray-600"
-                  }>
-                  Blog
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/projects'>
-                <a
-                  className={
-                    title === "Projects" ? "text-gray-800" : "text-gray-600"
-                  }>
-                  Projects
-                </a>
-              </Link>
-            </li>
-            <li>
-              <Link href='/contact'>
-                <a
-                  className={
-                    title === "Contact" ? "text-gray-800" : "text-gray-600"
-                  }>
-                  Contact
-                </a>
-              </Link>
-            </li>
-          </ul>
-        </div>
-        {children}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Zubair Mohsin | {title}</title>
+            </Head>
+            <header className="px-6 sm:px-12 lg:px-16 py-6 mb-12 lg:flex justify-between">
+                <h1 className="text-2xl">
+                    <Link href="/">
+                        <a>Zubair Mohsin</a>
+                    </Link>
+                </h1>
+                <nav>
+                    <ul className="flex justify-center lg:justify-end space-x-4">
+                        <li>
+                            <Link href="https://dev.to/zubairmohsin33">
+                                <a
+                                    target="_blank"
+                                    className={
+                                        title === 'Blog'
+                                            ? 'text-gray-800'
+                                            : 'text-gray-600'
+                                    }
+                                >
+                                    Blog
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/projects">
+                                <a
+                                    className={
+                                        title === 'Projects'
+                                            ? 'text-gray-800'
+                                            : 'text-gray-600'
+                                    }
+                                >
+                                    Projects
+                                </a>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/contact">
+                                <a
+                                    className={
+                                        title === 'Contact'
+                                            ? 'text-gray-800'
+                                            : 'text-gray-600'
+                                    }
+                                >
+                                    Contact
+                                </a>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
+            </header>
+            <div className="container max-w-2xl mx-auto px-4">{children}</div>
+        </>
+    );
 }
