36785a690ef9ba9b56e55a386749a7bdd035607a
Zubair Mohsin <zubairmohsin33@gmail.com>
2021-03-11 12:18:39 +0500

    WIP

--- components/Layout.js
+++ components/Layout.js
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 
 export default function Layout({ children, title }) {
     const router = useRouter();
+    const isProduction = process.env.NODE_ENV === 'production';
     return (
         <>
             <Head>
@@ -13,6 +14,22 @@ export default function Layout({ children, title }) {
                 />
                 <meta charSet="utf-8" />
                 <title>{title}</title>
+                {isProduction && (
+                    <script
+                        dangerouslySetInnerHTML={{
+                            __html: `
+                                (function(){
+                                    var t,i,e,n=window,o=document,a=arguments,s="script",r=["config","track","identify","visit","push","call","trackForm","trackClick"],c=function(){var t,i=this;for(i._e=[],t=0;r.length>t;t++)(function(t){i[t]=function(){return i._e.push([t].concat(Array.prototype.slice.call(arguments,0))),i}})(r[t])};for(n._w=n._w||{},t=0;a.length>t;t++)n._w[a[t]]=n[a[t]]=n[a[t]]||new c;i=o.createElement(s),i.async=1,i.src="//static.woopra.com/js/w.js",e=o.getElementsByTagName(s)[0],e.parentNode.insertBefore(i,e)
+                                })("woopra");
+
+                                woopra.config({
+                                    domain: 'zubairmohsin.dev'
+                                });
+                                woopra.track();
+                            `,
+                        }}
+                    ></script>
+                )}
             </Head>
             <header className="px-6 sm:px-12 lg:px-16 py-6 mb-12 lg:flex justify-between">
                 <h1 className="text-center text-2xl">
