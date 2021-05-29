e2e2fe1924ed9ebe1fdbed6627510dbf1687909c
Zubair Mohsin <zubairmohsin33@gmail.com>
2021-05-29 13:58:10 +0500

    SEO for landing page

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { Post } from '../components/Post';
 import { posts } from '../allPosts';
 import Layout from '../components/Layout';
+import { NextSeo } from 'next-seo';
 
 export default function Index() {
     const sortedPostsByDate = posts.sort((p1, p2) =>
@@ -9,6 +10,30 @@ export default function Index() {
 
     return (
         <Layout title="Zubair Mohsin">
+            <NextSeo
+                title="Zubair Mohsin | Blog"
+                description="Zubair Mohsin's personal blog about Laravel and Shopify App Development."
+                canonical="https://zubair.dev"
+                openGraph={{
+                    url: "https://zubair.dev",
+                    title: "Zubair Mohsin",
+                    description: "Zubair Mohsin's personal blog about Laravel and Shopify App Development.",
+                    site_name: 'Zubair Mohsin -  Blog',
+                    images: [
+                        {
+                            url:
+                                'https://og-image-gcojn5iwm-zubairmohsin.vercel.app/' +
+                                encodeURI(meta.title),
+                        },
+                    ],
+                    locale: 'en',
+                }}
+                twitter={{
+                    cardType: 'summary_large_image',
+                    handle: '@Zubairmohsin33',
+                    site: 'Zubair Mohsin - Blog',
+                }}
+            />
             {sortedPostsByDate.map((post) => (
                 <Post key={post.link} post={post} />
             ))}
